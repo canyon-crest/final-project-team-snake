b336a3dae76eba388883c61e3d35d8a075b755d2
BrandoGiorelli <102613905+BrandoGiorelli@users.noreply.github.com>
2025-05-27 00:18:01 -0700

    Update EnemyTank.java

--- src/EnemyTank.java
+++ src/EnemyTank.java
@@ -2,13 +2,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 class EnemyTank extends Tank {
-    int x, y, width, height, lives, energy;
-    Color color;
-
     public EnemyTank(int x, int y, Color color, int energy) {
-    	super(x,y,color,energy);
+        super(x, y, color, energy);
     }
 
+    @Override
     public void draw(Graphics g) {
         g.setColor(color);
         g.fillRect(x, y, width, height);
