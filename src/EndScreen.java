9f64c1b25a5a09ec47e8416628080b4b2f3527d6
BrandoGiorelli <102613905+BrandoGiorelli@users.noreply.github.com>
2025-05-27 23:11:01 -0700

    Update EndScreen.java

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -2,10 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+/**
+ * Represents the end-of-game screen that displays the winner and allows the user to play again or return to the main menu.
+ */
 public class EndScreen extends JPanel {
     private JButton playAgainButton;
     private JButton mainMenuButton;
 
+    /**
+     * Constructs the EndScreen panel with play again and main menu buttons.
+     * @param actionListener the ActionListener for button actions
+     */
     public EndScreen(ActionListener actionListener) {
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -33,7 +40,35 @@ public class EndScreen extends JPanel {
         add(mainMenuButton, gbc);
     }
 
+    /**
+     * Sets the winner label to display the name of the winner.
+     * @param winner the name of the winning player
+     */
     public void setWinner(String winner) {
         ((JLabel)getComponent(1)).setText(winner + " wins!");
+    }
+
+    /**
+     * Resets the winner label to an empty string.
+     * Useful when starting a new game or resetting the end screen.
+     */
+    public void resetWinnerLabel() {
+        ((JLabel)getComponent(1)).setText("");
+    }
+
+    /**
+     * Gets the "Play Again" button.
+     * @return the play again JButton
+     */
+    public JButton getPlayAgainButton() {
+        return playAgainButton;
+    }
+
+    /**
+     * Gets the "Main Menu" button.
+     * @return the main menu JButton
+     */
+    public JButton getMainMenuButton() {
+        return mainMenuButton;
     }
 }
