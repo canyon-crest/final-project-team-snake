4951dc8e660ad20506969a10a4b6aa1104528d33
BrandoGiorelli <102613905+BrandoGiorelli@users.noreply.github.com>
2025-05-28 19:23:38 -0700

    Update EndScreen.java

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -41,6 +41,50 @@ public class EndScreen extends JPanel {
     }
 
     /**
+    * Paints a custom night-themed background on the end screen.
+    * This includes a deep blue gradient sky, a soft full moon, randomly placed stars,
+    * and two layers of blue hills to create a calm night landscape.
+    *
+    * @param g the Graphics object used for drawing
+    */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Deep blue to navy night sky
+        Graphics2D g2 = (Graphics2D) g;
+        GradientPaint skyGrad = new GradientPaint(
+            0, 0, new Color(18, 30, 85),           // Deep navy
+            0, getHeight(), new Color(42, 60, 124)); // Blue
+        g2.setPaint(skyGrad);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+
+        // Draw a soft full moon
+        g.setColor(new Color(230, 230, 255, 200));
+        g.fillOval(getWidth()/2 - 70, 60, 100, 100);
+
+        // Star field (random, but not too many)
+        g.setColor(new Color(255,255,255,140));
+        for (int i = 0; i < 45; i++) {
+            int starX = (int)(Math.random() * getWidth());
+            int starY = (int)(Math.random() * (getHeight()/2));
+            g.fillOval(starX, starY, 2, 2);
+        }
+
+        // Darker distant hills
+        g.setColor(new Color(33, 51, 98, 220));
+        int[] x1 = {0, getWidth()/5, getWidth()/2, 4*getWidth()/5, getWidth()};
+        int[] y1 = {getHeight(), getHeight()-130, getHeight()-115, getHeight()-140, getHeight()};
+        g.fillPolygon(x1, y1, x1.length);
+
+        // Foreground hill (almost black blue)
+        g.setColor(new Color(20, 32, 53, 240));
+        int[] x2 = {0, getWidth()/3, getWidth()/2, 2*getWidth()/3, getWidth()};
+        int[] y2 = {getHeight(), getHeight()-55, getHeight()-65, getHeight()-60, getHeight()};
+        g.fillPolygon(x2, y2, x2.length);
+    }
+
+    /**
      * Sets the winner label to display the name of the winner.
      * @param winner the name of the winning player
      */
